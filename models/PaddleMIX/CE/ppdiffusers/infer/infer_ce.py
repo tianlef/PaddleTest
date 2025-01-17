f00a952dc5f4926f6bfb0ef4e2c9fa48dd1993ad
tianlef <1095012807@qq.com>
2025-01-17 19:20:46 +0800

    ppdiffusers inference

--- models/PaddleMIX/CE/ppdiffusers/infer/infer_ce.py
+++ models/PaddleMIX/CE/ppdiffusers/infer/infer_ce.py
@@ -6,100 +6,15 @@ import sys
 import json
 import pexpect
 # 假设我们有一个脚本列表
-def infer_process(executed_log_path, model_num):
-    scripts = [
-        "dual_text_and_image_guided_generation-versatile_diffusion.py",
-        "image_guided_image_inpainting-paint_by_example.py",
-        "image_inpainting-repaint.py",
-        "image_mixing-clip_guided_stable_diffusion.py",
-        "image_to_image_text_guided_generation-alt_diffusion.py",
-        "image_to_image_text_guided_generation-controlnet.py",
-        "image_to_image_text_guided_generation-deepfloyd_if.py",
-        "image_to_image_text_guided_generation-kandinsky2_2_controlnet.py",
-        "image_to_image_text_guided_generation-kandinsky2_2.py",
-        "image_to_image_text_guided_generation-kandinsky.py",
-        "image_to_image_text_guided_generation-stable_diffusion_2.py",
-        "image_to_image_text_guided_generation-stable_diffusion_3.py",
-        "image_to_image_text_guided_generation-stable_diffusion_controlnet.py",
-        "image_to_image_text_guided_generation-stable_diffusion.py",
-        "image_to_image_text_guided_generation-stable_diffusion_xl.py",
-        "image_to_text_generation-unidiffuser.py",
-        "image_variation-stable_diffusion.py",
-        "image_variation-unidiffuser.py",
-        "image_variation-versatile_diffusion.py",
-        "instruct_pix2pix-stable_diffusion_xl.py",
-        "super_resolution-latent_diffusion.py",
-        "text_guided_generation-semantic_stable_diffusion.py",
-        "text_guided_image_inpainting-deepfloyd_if.py",
-        "text_guided_image_inpainting-kandinsky2_2.py",
-        "text_guided_image_inpainting-kandinsky.py",
-        "text_guided_image_inpainting-stable_diffusion_2.py",
-        "text_guided_image_inpainting-stable_diffusion_controlnet.py",
-        "text_guided_image_inpainting-stable_diffusion.py",
-        "text_guided_image_inpainting-stable_diffusion_xl.py",
-        "text_guided_image_upscaling-stable_diffusion_2.py",
-        "text_to_3d_generation-shape_e.py",
-        "text_to_3d_generation-shape_e_image2image.py",
-        "text_to_audio_generation-audio_ldm.py",
-        "text_to_image_generation-alt_diffusion.py",
-        "text_to_image_generation-auto.py",
-        "text_to_image_generation-consistency_models.py",
-        "text_to_image_generation-deepfloyd_if.py",
-        "text_to_image_generation-kandinsky2_2_controlnet.py",
-        "text_to_image_generation-kandinsky2_2.py",
-        "text_to_image_generation-kandinsky.py",
-        "text_to_image_generation-latent_diffusion.py",
-        "text_to_image_generation_mixture_tiling-stable_diffusion.py",
-        "text_to_image_generation-sdxl_base_with_refiner.py",
-        "text_to_image_generation-stable_diffusion_2.py",
-        "text_to_image_generation-stable_diffusion_3.py",
-        "text_to_image_generation-stable_diffusion_controlnet.py",
-        "text_to_image_generation-stable_diffusion.py",
-        "text_to_image_generation-stable_diffusion_safe.py",
-        "text_to_image_generation-stable_diffusion_t2i_adapter.py",
-        "text_to_image_generation-stable_diffusion_xl_controlnet.py",
-        "text_to_image_generation-stable_diffusion_xl.py",
-        "text_to_image_generation-t2i-adapter.py",
-        "text_to_image_generation-unclip.py",
-        "text_to_image_generation-unidiffuser.py",
-        "text_to_image_generation-versatile_diffusion.py",
-        "text_to_image_generation-vq_diffusion.py",
-        "text_to_video_generation-lvdm.py",
-        "text_to_video_generation-synth_img2img.py",
-        "text_to_video_generation-synth.py",
-        "text_to_video_generation-zero.py",
-        "text_variation-unidiffuser.py",
-        "unconditional_audio_generation-audio_diffusion.py",
-        "unconditional_audio_generation-dance_diffusion.py",
-        "unconditional_audio_generation-spectrogram_diffusion.py",
-        "unconditional_image_generation-ddim.py",
-        "unconditional_image_generation-ddpm.py",
-        "unconditional_image_generation-latent_diffusion_uncond.py",
-        "unconditional_image_generation-pndm.py",
-        "unconditional_image_generation-score_sde_ve.py",
-        "unconditional_image_generation-stochastic_karras_ve.py",
-        "unconditional_image_text_joint_generation-unidiffuser.py",
-        "class_conditional_image_generation-dit.py",
-        "class_conditional_image_generation-large_dit_3b.py",
-        "class_conditional_image_generation-large_dit_7b.py",
-        "image_to_video_generation_image_to_video.py",
-        "image_to_video_generation_stable_video_diffusion.py",
-        "text_to_audio_generation-audio_ldm2.py",
-        "text_to_image_generation-latent_diffusion_uvit_small.py",
-        "text_to_image_generation_consistency_model.py",
-        "text_to_image_generation_kandinsky3.py",
-        "text_to_image_generation_largedit_3b.py",
-        "text_to_image_generation_wuerstchen.py",
-        "text_to_video_generation-synth.py",
-        "text_to_video_generation-synth_img2img.py",
-        "text_to_video_generation-zero.py",
-        "text_to_video_generation_animediff.py",
-        "unconditional_audio_generation-audio_diffusion.py",
-        "unconditional_audio_generation-dance_diffusion.py",
-        "video_to_video_generation_video_to_video.py",
-        "text_to_image_generation-stable_diffusion_3_5.py"
-    ]
+def generate_all_inference_scripts():
+    with open('./all.json', 'r', encoding='utf8') as f:
+        script_list = json.load(f)
+        keys = list(script_list.keys())
+    return keys
 
+def select_random_scripts(scripts, model_num, executed_log_path):
+    if model_num == 'all':
+        return scripts
     # 随机选择要执行的脚本数量（假设选择5个脚本）
     if os.path.isfile(executed_log_path):
         try:
@@ -114,21 +29,24 @@ def infer_process(executed_log_path, model_num):
     else:
         executed_dirs = set()
         current_epoch = 1
-
     remaining_dirs = list(set(scripts) - executed_dirs)
-
     if not remaining_dirs:
         print(f"All directories have been covered in epoch {current_epoch}. Starting a new epoch.")
         executed_dirs = set()
         remaining_dirs = list(scripts)
         current_epoch += 1
-
     selected_dirs = random.sample(remaining_dirs, min(model_num, len(remaining_dirs)))
-    
-
-    # 更新已执行的目录记录
+    # 打印选择的目录
     executed_dirs.update(selected_dirs)
+    print(f"Epoch {current_epoch}: Selected directories: {selected_dirs}")
+    # 更新已执行的目录记录
+    with open(executed_log_path, "w") as file:
+        json.dump({"executed_dirs": list(executed_dirs), "epoch": current_epoch}, file)
+        print(f"Epoch {current_epoch}: Selected directories: {selected_dirs}")
+        print(f"Executed models in this epoch {executed_dirs}")
+    return selected_dirs
 
+def infer_process(model_num, selected_dirs):
     # 定义路径
     root_path = os.getenv('root_path')  # 获取root_path环境变量
     work_path = os.path.join(root_path, 'PaddleMIX/ppdiffusers/examples/inference/')
@@ -144,11 +62,9 @@ def infer_process(executed_log_path, model_num):
     # 创建日志目录
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-
     # 复制文件
-    # 不对paddlenlp的版本进行限制
-    # shutil.copy(work_path3 + "/change_paddlenlp_version.sh", os.path.join(root_path, "PaddleMIX"))
-    # os.system('chmod +x ${root_path}/PaddleMIX/change_paddlenlp_version.sh')
+    # 执行的时候不再对paddlenlp的脚本进行更改 只在环境设置中更改paddlenlp的脚本
+   
     command = f"cp -rf ./* {work_path}/"
     subprocess.run(command, shell=True, check=True)
 
@@ -159,11 +75,6 @@ def infer_process(executed_log_path, model_num):
     subprocess.run(['pip', 'install', '-r', 'requirements.txt'], check=True)
     subprocess.run(['pip', 'install', '-e', '.'], check=True)
 
-    # 执行更改 PaddleNLP 版本的脚本
-   
-    # subprocess.run([os.path.join(root_path, 'PaddleMIX/change_paddlenlp_version.sh')], shell=True, check=True)
-
-    # 安装其他依赖
     subprocess.run(['pip', 'install', 'pytest', 'safetensors', 'ftfy', 'fastcore', 'opencv-python', 'einops', 'parameterized', 'requests-mock'], check=True)
     subprocess.run(['pip', 'install', 'ligo-segments'], check=True)
     subprocess.run(['pip', 'install', 'fastdeploy-gpu-python', '-f', 'https://www.paddlepaddle.org.cn/whl/fastdeploy.html'], check=True)
@@ -179,7 +90,7 @@ def infer_process(executed_log_path, model_num):
 
     # 执行接下来的操作（如模型推理等）
     exit_code = 0
-    print(f"Epoch {current_epoch}: Selected directories: {selected_dirs}")
+    
 
     # 你可以继续根据需要添加其他的 脚本执行或操作
     # 例如运行某些 文件
@@ -227,20 +138,13 @@ def infer_process(executed_log_path, model_num):
             print(f"******* Finished running {script} ***********")
     
     # 保存更新后的已执行目录和轮次
-    with open(executed_log_path, "w") as file:
-        json.dump({"executed_dirs": list(executed_dirs), "epoch": current_epoch}, file)
-        print(f"Epoch {current_epoch}: Selected directories: {selected_dirs}")
-        print(f"Executed models in this epoch {executed_dirs}")
-
     # 输出最终的 exit_code
     print(f"Final exit code: {exit_code}")
-
      # 查看结果
     ce_res_log_path = os.path.join(log_dir, "infer_res.log")
     if os.path.isfile(ce_res_log_path):
         with open(ce_res_log_path, "r") as log_file:
             print(log_file.read())
-    
     # 退出脚本
     exit(exit_code)
 
@@ -248,8 +152,12 @@ def infer_process(executed_log_path, model_num):
 if __name__ == '__main__':
     try:
         print("Starting script...")
+        scripts = generate_all_inference_scripts()
+        # print(scripts)
+        print(f"Total number of inference scripts found: {len(scripts)}")
         record_path = sys.argv[1]
-        model_num = int(sys.argv[2]) if len(sys.argv) > 2 else 5
-        infer_process(record_path, model_num)
+        model_num = int(sys.argv[2])
+        selected_scripts = select_scripts(scripts, model_num)
+        infer_process(record_path, model_num, scripts)
     except Exception as e:
         print(e)
