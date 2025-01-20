3e2b30e6cb60520d5cc5880ee388a13775ef9a98
tianlef <1095012807@qq.com>
2025-01-20 15:38:36 +0800

    cinn trainging sd1.5

---
+++ models/PaddleMIX/CE/cinn_training/stable_diffusion/muti_train.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+rm -rf ./laion400m_pretrain_output_trainer
+export FLAGS_conv_workspace_size_limit=4096
+# 是否开启ema
+export FLAG_USE_EMA=0
+# 是否开启recompute
+export FLAG_RECOMPUTE=1
+# 是否开启xformers
+export FLAG_XFORMERS=1
+export FLAGS_prim_all=true;export FLAGS_prim_enable_dynamic=true;export FLAGS_use_cinn=true;export MIN_GRAPH_SIZE=0;export FLAGS_prim_forward_blacklist="pd_op.dropout"
+python -u -m paddle.distributed.launch --gpus "0,1" train_txt2img_laion400m_trainer.py \
+  --do_train \
+  --output_dir ./laion400m_pretrain_output_trainer \
+  --per_device_train_batch_size 2 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 1e-4 \
+  --weight_decay 0.01 \
+  --max_steps 100 \
+  --lr_scheduler_type "constant" \
+  --warmup_steps 0 \
+  --image_logging_steps 100 \
+  --logging_steps 10 \
+  --resolution 256 \
+  --save_steps 100 \
+  --save_total_limit 20 \
+  --seed 23 \
+  --dataloader_num_workers 4 \
+  --vae_name_or_path CompVis/stable-diffusion-v1-4/vae \
+  --text_encoder_name_or_path CompVis/stable-diffusion-v1-4/text_encoder \
+  --unet_name_or_path ./sd/unet_config.json \
+  --file_list ./data/filelist/train.filelist.list \
+  --model_max_length 77 \
+  --max_grad_norm -1 \
+  --disable_tqdm True \
+  --bf16 False
+
+# 检查命令是否成功执行
+if [ $? -ne 0 ]; then
+  exit 1
+fi
