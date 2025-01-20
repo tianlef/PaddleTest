aa452e68e155474bc4514df9aed373cfdb98dc3c
tianlef <1095012807@qq.com>
2025-01-20 19:33:49 +0800

    frametest llava

--- models/PaddleMIX/Framework/pir_training/llava/start.sh
+++ models/PaddleMIX/Framework/pir_training/llava/start.sh
@@ -25,19 +25,6 @@ bash prepare.sh
 export FLAGS_use_cuda_managed_memory=true
 export FLAGS_allocator_strategy=auto_growth
 
-# infer暂时没有达到自动化测试，需要手动测试
-# echo "*******paddlemix llava infer***********"
-# (python paddlemix/examples/llava/run_predict_multiround.py \
-#     --model-path "paddlemix/llava/llava-v1.5-7b" \
-#     --image-file "https://bj.bcebos.com/v1/paddlenlp/models/community/GroundingDino/000000004505.jpg") 2>&1 | tee ${log_dir}/paddlemix_llava_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava infer run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava infer run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava infer end***********"
 
 echo "*******paddlemix llava finetune***********"
 # llava pretain 有报错
