4b2c2ff9960d89e1aa66fe11d37e14cb050f7856
tianlef <1095012807@qq.com>
2025-01-21 17:42:47 +0800

    dit fix

--- models/PaddleMIX/Framework/pir_training/class_conditional_image_generation_DiT/start.sh
+++ models/PaddleMIX/Framework/pir_training/class_conditional_image_generation_DiT/start.sh
@@ -69,42 +69,5 @@ echo "*******class_conditional_image_generation/DiT large train end***********"
 
 
 
-echo "*******class_conditional_image_generation/DiT infer begin***********"
-(python infer_demo_dit.py) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_infer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "class_conditional_image_generation/DiT infer run success" >>"${log_dir}/ce_res.log"
-else
-    echo "class_conditional_image_generation/DiT infer run fail" >>"${log_dir}/ce_res.log"
-fi
-
-echo "*******class_conditional_image_generation/DiT transfer begin***********"
-(python tools/convert_dit_to_ppdiffusers.py \
-    --image_size 256 \
-    --model_name DiT_XL_2 \
-    --model_weights output_trainer/DiT_XL_patch2_trainer/model_state.pdparams \
-    --checkpoint_path output_trainer/DiT_XL_patch2_trainer/checkpoint-10/) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_transfer.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "class_conditional_image_generation/DiT transfer run success" >>"${log_dir}/ce_res.log"
-else
-    echo "class_conditional_image_generation/DiT transfer run fail" >>"${log_dir}/ce_res.log"
-fi
-
-
-echo "*******class_conditional_image_generation/DiT infer_generate begin***********"
-(python generate_infer_test.py) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_infer_generate.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "class_conditional_image_generation/DiT infer_generate run success" >>"${log_dir}/ce_res.log"
-else
-    echo "class_conditional_image_generation/DiT infer_generate run fail" >>"${log_dir}/ce_res.log"
-fi
-
-
-
 echo exit_code:${exit_code}
 exit ${exit_code}
