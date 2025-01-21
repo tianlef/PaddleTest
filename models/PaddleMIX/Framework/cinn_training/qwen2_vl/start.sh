792906538dbd0e957dae9aa6c8be74577ba83477
tianlef <1095012807@qq.com>
2025-01-21 11:21:30 +0800

    qwen2vl log

--- models/PaddleMIX/Framework/cinn_training/qwen2_vl/start.sh
+++ models/PaddleMIX/Framework/cinn_training/qwen2_vl/start.sh
@@ -6,7 +6,7 @@ echo ${cur_path}
 work_path=${root_path}/PaddleMIX
 echo ${work_path}
 
-log_dir=${root_path}/pir_log
+log_dir=${root_path}/cinn_log
 
 if [ ! -d "$log_dir" ]; then
     mkdir -p "$log_dir"
