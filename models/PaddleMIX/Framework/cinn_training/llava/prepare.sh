1aed861a19c1d145a5fb159b37109d3ed392da7a
tianlef <1095012807@qq.com>
2025-01-22 19:14:21 +0800

    fix llava

--- models/PaddleMIX/Framework/cinn_training/llava/prepare.sh
+++ models/PaddleMIX/Framework/cinn_training/llava/prepare.sh
@@ -1,4 +1,14 @@
 # 下载数据集
+
+rm -rf llava_bench_data.tar
+rm -rf /root/.paddlemix/datasets/llava_bench_data
+# dataset
+wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/benchmark/llava_bench_data.tar
+tar -xf llava_bench_data.tar
+mv llava_bench_data /root/.paddlemix/datasets/
+rm -rf llava_bench_data.tar
+ln -s /root/.paddlemix/datasets/llava_bench_data ./
+
 if [ -e "ScienceQA.tar" ]; then
     tar -xf ScienceQA.tar
     echo "文件存在"
