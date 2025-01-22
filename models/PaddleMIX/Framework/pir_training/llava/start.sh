2b5f71a81e051909f5a151b3bc50d66be5b85a2e
tianlef <1095012807@qq.com>
2025-01-22 16:25:37 +0800

    llava training

--- models/PaddleMIX/Framework/pir_training/llava/start.sh
+++ models/PaddleMIX/Framework/pir_training/llava/start.sh
@@ -61,6 +61,29 @@ else
 fi
 echo "*******paddlemix llava lora end***********"
 
+echo "*******paddlemix llava sft 1.5***********"
+(python  -u check_loss.py "python paddlemix/tools/supervised_finetune.py llava_v100_sft_1.5.json") 2>&1 | tee ${log_dir}/paddlemix_llava_sft_1.5.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava sft 1.5 run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava sft 1.5  run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava 1.5 sft end***********"
+
+echo "*******paddlemix llava lora 1.5 ***********"
+
+(python  -u check_loss.py "python paddlemix/tools/supervised_finetune.py llava_v100_lora_1.5.json") 2>&1 | tee ${log_dir}/paddlemix_llava_lora.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava lora 1.5 run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava lora 1.5 run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava lora 1.5 end***********"
+
 unset FLAGS_use_cuda_managed_memory
 unset FLAGS_allocator_strategy
 
