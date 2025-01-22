aab4320933f8037aa74598ed3dc85f1387709f4e
tianlef <1095012807@qq.com>
2025-01-22 20:07:10 +0800

    llava 1.5 inference

---
+++ models/PaddleMIX/Framework/pir_inference/llava1.5/export.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+log_dir=${root_path}/deploy_log
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+
+export PYTHONPATH=${root_path}/PaddleNLP/:${root_path}/PaddleMIX
+
+export FLAGS_enable_pir_api=0
+
+(python deploy/llava/export_model.py \
+    --model_name_or_path "liuhaotian/llava-v1.5-7b" \
+    --save_path "./llava_static_old" \
+    --encode_image \
+    --fp16) 2>&1 | tee ${log_dir}/llava_export_oldir0.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "llava_export_oldir 0  success" >>"${log_dir}/ce_res.log"
+else
+    echo "llava_export_oldir 0  fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******llava_export_oldir 0 oldir end***********"
+
+(python deploy/llava/export_model.py \
+    --model_name_or_path "liuhaotian/llava-v1.5-7b" \
+    --save_path "./llava_static" \
+    --encode_text \
+    --fp16) 2>&1 | tee ${log_dir}/llava_export_oldir1.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "llava_export_oldir 1 success" >>"${log_dir}/ce_res.log"
+else
+    echo "llava_export_oldir 1  fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******llava_export_oldir 1  end***********"
+
+export FLAGS_enable_pir_api=1
+
+
+(python deploy/llava/export_model.py \
+    --model_name_or_path "liuhaotian/llava-v1.5-7b" \
+    --save_path "./llava_static_new" \
+    --encode_image \
+    --fp16) 2>&1 | tee ${log_dir}/llava_export_new0.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "llava_export_new 0  success" >>"${log_dir}/ce_res.log"
+else
+    echo "llava_export_new 0  fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******llava_export_new 0 oldir end***********"
+
+(python deploy/llava/export_model.py \
+    --model_name_or_path "liuhaotian/llava-v1.5-7b" \
+    --save_path "./llava_static_new" \
+    --encode_text \
+    --fp16) 2>&1 | tee ${log_dir}/llava_export_new1.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "llava_export_new 1 success" >>"${log_dir}/ce_res.log"
+else
+    echo "llava_export_new 1  fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******llava_export_new 1  end***********"
+
+unset FLAGS_enable_pir_api
+echo exit_code:${exit_code}
+exit ${exit_code}
---
+++ models/PaddleMIX/Framework/pir_inference/llava1.5/infer_script_new.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python deploy/llava/run_static_predict.py --model_name_or_path "liuhaotian/llava-v1.5-7b" \
+--image_file "https://bj.bcebos.com/v1/paddlenlp/models/community/GroundingDino/000000004505.jpg" \
+--first_model_path "llava_static_new/encode_image/clip"  \
+--second_model_path "llava_static_new/encode_text/llama" \
+--fp16
