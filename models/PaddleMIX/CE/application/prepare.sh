7544cbdb8b6419520b2b0b11327fa0f944c881c4
tianlef <1095012807@qq.com>
2025-01-24 00:04:44 +0800

    application

--- models/PaddleMIX/CE/application/prepare.sh
+++ models/PaddleMIX/CE/application/prepare.sh
@@ -21,6 +21,3 @@ unset http_proxy
 unset https_proxy
 
 wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/audio-files/zh.wav
-
- python -m pip install paddlepaddle-gpu==3.0.0b1 -i https://www.paddlepaddle.org.cn/packages/stable/cu118/
- pip install paddlenlp==3.0.0b1
