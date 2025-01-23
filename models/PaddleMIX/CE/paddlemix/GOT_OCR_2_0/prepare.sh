55d32efe4da9cba526ab255ff5db7b5e44db5f7b
tianlef <1095012807@qq.com>
2025-01-23 21:09:12 +0800

    GOT_OCR_2_0

--- models/PaddleMIX/CE/paddlemix/GOT_OCR_2_0/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/GOT_OCR_2_0/prepare.sh
@@ -2,6 +2,8 @@ cd ${root_path}/paddlemix/examples/GOT_OCR_2_0
 pip install -r requirements.txt
 
 mix_path=${root_path}/PaddleMIX
+rm -rf synthdog_en.tar 
+rm -rf synthdog_en
 wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/synthdog_en.tar # 2.4G
 tar -xvf synthdog_en.tar
 # 数据集下载
