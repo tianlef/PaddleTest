c16cf99b0134801e9c33804e784282eb7e1f56bc
tianlef <1095012807@qq.com>
2025-01-23 21:17:03 +0800

    GOT_OCR_2_0

--- models/PaddleMIX/CE/paddlemix/GOT_OCR_2_0/start.sh
+++ models/PaddleMIX/CE/paddlemix/GOT_OCR_2_0/start.sh
@@ -21,89 +21,89 @@ fi
 bash prepare.sh
 exit_code=0
 
-cd ${work_path}
-model_name=GOT_OCR_2_0
+# cd ${work_path}
+# model_name=GOT_OCR_2_0
 
-case_name=plain_texts_OCR
-echo "******* ${model_name}_${case_name} begin***********"
-(python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
-  --model_name_or_path stepfun-ai/GOT-OCR2_0 \
-  --image_file paddlemix/demo_images/hospital.jpeg \
-  --ocr_type ocr \
-  --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "******* ${model_name}_${case_name} end***********"
-
-
-case_name=format_texts_OCR
-echo "******* ${model_name}_${case_name} begin***********"
-(python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
-  --model_name_or_path stepfun-ai/GOT-OCR2_0 \
-  --image_file paddlemix/demo_images/hospital.jpeg \
-  --ocr_type format \
-  --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "******* ${model_name}_${case_name} end***********"
+# case_name=plain_texts_OCR
+# echo "******* ${model_name}_${case_name} begin***********"
+# (python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
+#   --model_name_or_path stepfun-ai/GOT-OCR2_0 \
+#   --image_file paddlemix/demo_images/hospital.jpeg \
+#   --ocr_type ocr \
+#   --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "******* ${model_name}_${case_name} end***********"
 
 
+# case_name=format_texts_OCR
+# echo "******* ${model_name}_${case_name} begin***********"
+# (python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
+#   --model_name_or_path stepfun-ai/GOT-OCR2_0 \
+#   --image_file paddlemix/demo_images/hospital.jpeg \
+#   --ocr_type format \
+#   --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "******* ${model_name}_${case_name} end***********"
 
-case_name=multi_crop_plain_texts_OCR
-echo "******* ${model_name}_${case_name} begin***********"
-(python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
-  --model_name_or_path stepfun-ai/GOT-OCR2_0 \
-  --image_file paddlemix/demo_images/hospital.jpeg \
-  --ocr_type ocr \
-  --multi_crop \
-  --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "******* ${model_name}_${case_name} end***********"
 
-case_name=train
-echo "******* ${model_name}_${case_name} begin***********"
-(sh paddlemix/examples/GOT_OCR_2_0/run_train.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "******* ${model_name}_${case_name} end***********"
 
-case_name=after_train_infer
-echo "******* ${model_name}_${case_name} begin***********"
-(python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
-  --model_name_or_path work_dirs/got_ocr_20/ \
-  --image_file paddlemix/demo_images/hospital.jpeg \
-  --ocr_type ocr) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "******* ${model_name}_${case_name} end***********"
+# case_name=multi_crop_plain_texts_OCR
+# echo "******* ${model_name}_${case_name} begin***********"
+# (python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
+#   --model_name_or_path stepfun-ai/GOT-OCR2_0 \
+#   --image_file paddlemix/demo_images/hospital.jpeg \
+#   --ocr_type ocr \
+#   --multi_crop \
+#   --dtype "bfloat16") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "******* ${model_name}_${case_name} end***********"
 
-echo exit_code:${exit_code}
+# case_name=train
+# echo "******* ${model_name}_${case_name} begin***********"
+# (sh paddlemix/examples/GOT_OCR_2_0/run_train.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "******* ${model_name}_${case_name} end***********"
 
-# cat ${log_dir}/ce_res.log
-exit ${exit_code}
+# case_name=after_train_infer
+# echo "******* ${model_name}_${case_name} begin***********"
+# (python paddlemix/examples/GOT_OCR_2_0/got_ocr2_0_infer.py \
+#   --model_name_or_path work_dirs/got_ocr_20/ \
+#   --image_file paddlemix/demo_images/hospital.jpeg \
+#   --ocr_type ocr) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "******* ${model_name}_${case_name} end***********"
+
+# echo exit_code:${exit_code}
+
+# # cat ${log_dir}/ce_res.log
+# exit ${exit_code}
