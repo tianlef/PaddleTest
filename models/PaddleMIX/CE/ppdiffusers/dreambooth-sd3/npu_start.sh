ed4b79106408baca09ac0866a8526633bc2e84f8
tianlef <1095012807@qq.com>
2025-02-24 22:49:06 +0800

    dreambooth sd3 npu

--- models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/npu_start.sh
+++ models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/npu_start.sh
@@ -27,31 +27,31 @@ export FLAGS_npu_scale_aclnn=True
 export FLAGS_allocator_strategy=auto_growth
 export USE_PEFT_BACKEND=True
 
-# # 单机训练
-# echo "*******dreambooth train begin***********"
-# (bash train.sh) 2>&1 | tee ${log_dir}/dreambooth_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "dreambooth train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "dreambooth train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******dreambooth train end***********"
+# 单机训练
+echo "*******dreambooth train begin***********"
+(bash train.sh) 2>&1 | tee ${log_dir}/dreambooth_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "dreambooth train run success" >>"${log_dir}/ce_res.log"
+else
+    echo "dreambooth train run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******dreambooth train end***********"
 
 
 
-# # 单机推理
-# echo "*******dreambooth infer begin***********"
-# (python infer.py) 2>&1 | tee ${log_dir}/dreambooth_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "dreambooth infer run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "dreambooth infer run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******dreambooth infer end***********"
+# 单机推理
+echo "*******dreambooth infer begin***********"
+(python infer.py) 2>&1 | tee ${log_dir}/dreambooth_infer.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "dreambooth infer run success" >>"${log_dir}/ce_res.log"
+else
+    echo "dreambooth infer run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******dreambooth infer end***********"
 
 # Lora训练
 echo "*******dreambooth lora train begin***********"
