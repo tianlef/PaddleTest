a36623247873e64635be4f093efb50a6327c6642
tianlef <1095012807@qq.com>
2025-03-10 20:31:38 +0800

    offline wandb for text_to_image sdxl

--- models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/gpu_train.sh
+++ models/PaddleMIX/CE/ppdiffusers/text_to_image_sdxl/gpu_train.sh
@@ -18,7 +18,6 @@ python -u train_text_to_image_sdxl.py \
   --max_train_steps=10000 \
   --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
-  --report_to="wandb" \
   --validation_prompt="a cute Sundar Pichai creature" --validation_epochs 5 \
   --checkpointing_steps=5000 \
   --output_dir="sdxl-pokemon-model"
