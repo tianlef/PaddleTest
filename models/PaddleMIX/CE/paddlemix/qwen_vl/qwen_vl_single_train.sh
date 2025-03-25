f721607177df6c6d5b78fc9a460d13bca5c3c187
tianlef <1095012807@qq.com>
2025-03-25 15:38:01 +0800

    fix qwen vl

--- models/PaddleMIX/CE/paddlemix/qwen_vl/qwen_vl_single_train.sh
+++ models/PaddleMIX/CE/paddlemix/qwen_vl/qwen_vl_single_train.sh
@@ -1,7 +1,7 @@
 MODEL_NAME="qwen-vl/qwen-vl-chat-7b"
 MASTER='127.0.0.1:8080'
 DATA="sft_examples.json"
-python paddlemix/examples/qwen_vl/finetune.py
+python paddlemix/examples/qwen_vl/finetune.py \
     --model_name_or_path ${MODEL_NAME} \
     --data_path ${DATA} \
     --dtype 'float16' \
