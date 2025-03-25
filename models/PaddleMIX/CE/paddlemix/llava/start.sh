f4fb5ca366d5b8cd20b5e3b93633c8788e178e11
tianlef <1095012807@qq.com>
2025-03-25 19:49:57 +0800

    fix llava

--- models/PaddleMIX/CE/paddlemix/llava/start.sh
+++ models/PaddleMIX/CE/paddlemix/llava/start.sh
@@ -26,53 +26,53 @@ export FLAGS_use_cuda_managed_memory=true
 export FLAGS_allocator_strategy=auto_growth
 
 
-# echo "*******paddlemix llava finetune***********"
-# # llava pretain 有报错
-# (python  -u check_loss.py "python paddlemix/examples/llava/pretrain.py llava_v100_pretrain.json") 2>&1 | tee ${log_dir}/paddlemix_llava_finetune.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava finetune run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava finetune run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava finetune end***********"
+echo "*******paddlemix llava finetune***********"
+# llava pretain 有报错
+(python  -u check_loss.py "python paddlemix/examples/llava/pretrain.py llava_v100_pretrain.json") 2>&1 | tee ${log_dir}/paddlemix_llava_finetune.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava finetune run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava finetune run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava finetune end***********"
 
-# echo "*******paddlemix llava sft***********"
-# (python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_sft.json") 2>&1 | tee ${log_dir}/paddlemix_llava_sft.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava sft run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava sft run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava sft end***********"
+echo "*******paddlemix llava sft***********"
+(python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_sft.json") 2>&1 | tee ${log_dir}/paddlemix_llava_sft.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava sft run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava sft run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava sft end***********"
 
-# echo "*******paddlemix llava lora***********"
+echo "*******paddlemix llava lora***********"
 
-# (python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_lora.json") 2>&1 | tee ${log_dir}/paddlemix_llava_lora.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava lora run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava lora run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava lora end***********"
+(python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_lora.json") 2>&1 | tee ${log_dir}/paddlemix_llava_lora.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava lora run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava lora run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava lora end***********"
 
-# echo "*******paddlemix llava sft 1.5***********"
-# (python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_sft_1.5.json") 2>&1 | tee ${log_dir}/paddlemix_llava_sft_1.5.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "paddlemix llava sft 1.5 run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "paddlemix llava sft 1.5  run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******paddlemix llava 1.5 sft end***********"
+echo "*******paddlemix llava sft 1.5***********"
+(python  -u check_loss.py "python paddlemix/examples/llava/supervised_finetune.py llava_v100_sft_1.5.json") 2>&1 | tee ${log_dir}/paddlemix_llava_sft_1.5.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "paddlemix llava sft 1.5 run success" >>"${log_dir}/ce_res.log"
+else
+    echo "paddlemix llava sft 1.5  run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******paddlemix llava 1.5 sft end***********"
 
-# echo "*******paddlemix llava lora 1.5 ***********"
+echo "*******paddlemix llava lora 1.5 ***********"
 
 (python paddlemix/examples/llava/supervised_finetune.py llava_v100_lora_1.5.json) 2>&1 | tee ${log_dir}/paddlemix_llava_lora.log
 tmp_exit_code=${PIPESTATUS[0]}
