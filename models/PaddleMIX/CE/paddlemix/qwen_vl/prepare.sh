40118cacf2f08019fb0d51cf0c8b9354dbe40b89
tianlef <1095012807@qq.com>
2025-03-25 10:37:00 +0800

    fix qwen vl

--- models/PaddleMIX/CE/paddlemix/qwen_vl/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/qwen_vl/prepare.sh
@@ -2,4 +2,5 @@ pip install tiktoken
 pip install -r paddlemix/appflow/requirements.txt
 pip install pexpect
 
+rm -rf sft_examples.json
 wget https://bj.bcebos.com/v1/paddlenlp/models/community/qwen-vl/sft_examples.json
