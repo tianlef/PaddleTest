63d1ada199460567649086bdd4a94a5f6b0de7f1
tianlef <1095012807@qq.com>
2025-03-27 22:13:50 +0800

    qwen2vl

--- models/PaddleMIX/A100/paddlemix/qwen2_vl/start.sh
+++ models/PaddleMIX/A100/paddlemix/qwen2_vl/start.sh
@@ -113,7 +113,7 @@ echo "******* ${model_name}_${case_name} end***********"
 
 
 echo "*******paddlemix qwen2_vl_sft_train begin begin***********"
-(sh paddlemix/examples/qwen2_vl/shell/baseline_2b_bs32_1e8.sh) 2>&1 | tee ${log_dir}/qwen2_vl_sft_train.log
+(sh baseline_2b_bs32_1e8.sh) 2>&1 | tee ${log_dir}/qwen2_vl_sft_train.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -164,7 +164,7 @@ echo "******* ${model_name}_${case_name} end***********"
 
 case_name=lora_train
 echo "******* ${model_name}_${case_name} begin***********"
-(sh paddlemix/examples/qwen2_vl/shell/baseline_2b_lora_bs32_1e8.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+(sh baseline_2b_lora_bs32_1e8.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
