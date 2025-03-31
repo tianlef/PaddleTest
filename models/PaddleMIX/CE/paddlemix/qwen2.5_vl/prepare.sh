a25247d14636e61748c05dec67e6cb97cba21af2
tianlef <1095012807@qq.com>
2025-03-31 10:40:13 +0800

    qwen2.5 vl

--- models/PaddleMIX/CE/paddlemix/qwen2.5_vl/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/qwen2.5_vl/prepare.sh
@@ -3,13 +3,9 @@ mix_path=${root_path}/PaddleMIX
 
 # 数据集下载
 cd ${mix_path}
-rm -rf playground
-rm -rf playground.tar
-wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground.tar --no-proxy
+rm -rf playground LaTeX_OCR
+rm -rf playground.tar LaTeX_OCR.tar
+wget https://paddlenlp.bj.bcebos.com/models/community/paddlemix/benchmark/playground.tar # 1.0G
+wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/LaTeX_OCR.tar # 1.7G
 tar -xf playground.tar
-
-cd playground
-wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource_json.tar --no-proxy
-tar -xf opensource_json.tar
-
-
+tar -xf LaTeX_OCR.tar
