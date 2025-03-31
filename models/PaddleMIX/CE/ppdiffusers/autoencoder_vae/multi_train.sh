edc51c8e420b09ec4ba7c7693e0e3b8a6ffaea47
tianlef <1095012807@qq.com>
2025-03-31 17:13:50 +0800

    ppdiffusers autoencoder vae

--- models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/multi_train.sh
+++ models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/multi_train.sh
@@ -8,12 +8,12 @@ python -u -m paddle.distributed.launch --gpus "0,1" train_vae.py \
   --vae_config_file config/vae.json \
   --freeze_encoder \
   --input_size 256 256 \
-  --max_train_steps 10 \
+  --max_train_steps 100 \
   --learning_rate 1e-4 \
   --batch_size 1 \
   --num_workers 2 \
   --logging_steps 50 \
-  --save_steps 10 \
+  --save_steps 100 \
   --image_logging_steps 50 \
   --disc_start 50 \
   --kl_weight 0.000001 \
--- models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/single_train_from_zero.sh
+++ models/PaddleMIX/CE/ppdiffusers/autoencoder_vae/single_train_from_zero.sh
@@ -4,7 +4,7 @@ python -u train_vae.py \
     --from_scratch \
     --vae_config_file config/vae.json \
     --input_size 256 256 \
-    --max_train_steps 10 \
+    --max_train_steps 100 \
     --learning_rate 1e-4 \
     --batch_size 4 \
     --num_workers 8 \
