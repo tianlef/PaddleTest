f3ff102660d3818550f11521225a51b720ef8b75
tianlef <1095012807@qq.com>
2025-04-01 19:58:22 +0800

    fix qwen deploy

--- models/PaddleMIX/CE/paddlemix/deploy/qwen_deploy.sh
+++ models/PaddleMIX/CE/paddlemix/deploy/qwen_deploy.sh
@@ -21,40 +21,40 @@ cd ${root_path}/PaddleMIX/
 cd ${work_path}
 # bash prepare_qwen.sh
 
-echo "*******paddlemix deploy qwen_ql begin***********"
+echo "*******paddlemix deploy qwen_vl begin***********"
 
 #静态图模型导出
-(bash qwen_export.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_ql_export.log
+(bash qwen_export.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_vl_export.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "paddlemix deploy qwen_ql export run success" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl export run success" >>"${log_dir}/ce_res.log"
 else
-    echo "paddlemix deploy qwen_ql export run fail" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl export run fail" >>"${log_dir}/ce_res.log"
 fi
 
 #转出静态图推理所需的语言模型
-(bash qwen_export_language.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_ql_language.log
+(bash qwen_export_language.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_vl_language.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "paddlemix deploy qwen_ql language run success" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl language run success" >>"${log_dir}/ce_res.log"
 else
-    echo "paddlemix deploy qwen_ql language run fail" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl language run fail" >>"${log_dir}/ce_res.log"
 fi
 
 cd ${work_path}
 #转出静态图推理所需的语言模型
-(bash qwen_predict.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_ql_predict.log
+(bash qwen_predict.sh) 2>&1 | tee ${log_dir}/run_deploy_qwen_vl_predict.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "paddlemix deploy qwen_ql predict run success" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl predict run success" >>"${log_dir}/ce_res.log"
 else
-    echo "paddlemix deploy qwen_ql predict run fail" >>"${log_dir}/ce_res.log"
+    echo "paddlemix deploy qwen_vl predict run fail" >>"${log_dir}/ce_res.log"
 fi
 
-echo "*******paddlemix deploy qwen_ql end***********"
+echo "*******paddlemix deploy qwen_vl end***********"
 
 # 检查命令是否成功执行
 if [ ${exit_code} -ne 0 ]; then
