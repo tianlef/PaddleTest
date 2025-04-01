8f04df6810424d04d8153afe141cd95bd4fd02da
tianlef <1095012807@qq.com>
2025-04-01 19:21:11 +0800

    fix eva02

--- models/PaddleMIX/CE/paddlemix/eva02/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/eva02/prepare.sh
@@ -2,13 +2,14 @@
 
 rm -rf /root/.paddlemix/datasets/*
 
-export http_proxy=${proxy}
-export https_proxy=${proxy}
-cd ${root_path}/PaddleMIX/paddlemix/external_ops/
-python setup.py install
-pip install -U numpy==1.23.5
-unset http_proxy
-unset https_proxy
+cd ${root_path}/
+rm -rf dataset
+mkdir dataset
+cd dataset
+# eva02 
+
+wget https://bj.bcebos.com/v1/paddlenlp/datasets/paddlemix/ILSVRC2012/ILSVRC2012_tiny.tar
+tar -xf ILSVRC2012_tiny.tar
 
 cd ${root_path}/PaddleMIX/paddlemix/examples/eva02/
 wget https://bj.bcebos.com/v1/paddlenlp/models/community/paddlemix/EVA/EVA02/eva02_Ti_pt_in21k_p14/model_state.pdparams
