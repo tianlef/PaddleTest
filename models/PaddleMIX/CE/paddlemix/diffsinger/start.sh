a155eeeb9422b579a7db8d015141e08bf3ce29ad
tianlef <1095012807@qq.com>
2025-04-03 19:25:01 +0800

    fix diffusers

--- models/PaddleMIX/CE/paddlemix/diffsinger/start.sh
+++ models/PaddleMIX/CE/paddlemix/diffsinger/start.sh
@@ -24,7 +24,7 @@ model_name=diffsinger
 
 case_name=predict
 
-cd ${work_path}/paddlemix/examples/diffsinger
+cd ${work_path}/examples/paddlemix/examples/diffsinger
 pip install -r requirements.txt
 echo "******* ${model_name}_${case_name} begin***********"
 (bash run_predict.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
