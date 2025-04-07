a6febf80707f56485ba69bec7b9611c9c01429bd
tianlef <1095012807@qq.com>
2025-04-07 11:24:16 +0800

    fix dreambooth dog prerare

--- models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/prepare.sh
@@ -5,7 +5,7 @@ pip install visualdl
 pip install -r requirements_sd3.txt
 
 
-
+rm -rf dog.zip
+rm -rf dog
 wget https://paddlenlp.bj.bcebos.com/models/community/westfish/develop-sdxl/dog.zip
 unzip -o dog.zip
-rm -rf dog.zip
