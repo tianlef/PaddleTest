98577f01184e0dfc91c6a2a997629297893cd9d3
tianlef <1095012807@qq.com>
2025-04-14 14:34:09 +0800

    fix dreambooth lora npu

--- models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/npu_start.sh
+++ models/PaddleMIX/CE/ppdiffusers/dreambooth-sd3/npu_start.sh
@@ -27,31 +27,7 @@ export FLAGS_npu_scale_aclnn=True
 export FLAGS_allocator_strategy=auto_growth
 export USE_PEFT_BACKEND=True
 
-# # 单机训练
-# echo "*******dreambooth train begin***********"
-# (bash train.sh) 2>&1 | tee ${log_dir}/dreambooth_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "dreambooth train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "dreambooth train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******dreambooth train end***********"
 
-
-
-# # 单机推理
-# echo "*******dreambooth infer begin***********"
-# (python infer.py) 2>&1 | tee ${log_dir}/dreambooth_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "dreambooth infer run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "dreambooth infer run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******dreambooth infer end***********"
 
 # Lora训练
 echo "*******dreambooth lora train begin***********"
@@ -66,8 +42,9 @@ fi
 echo "*******dreambooth lora train end***********"
 
 # Lora推理
+
 echo "*******dreambooth lora infer begin***********"
-(python lora_infer.py) 2>&1 | tee ${log_dir}/dreambooth_lora_infer.log
+(export USE_PEFT_BACKEND=True && python lora_infer.py) 2>&1 | tee ${log_dir}/dreambooth_lora_infer.log
 tmp_exit_code=${PIPESTATUS[0]}
 exit_code=$(($exit_code + ${tmp_exit_code}))
 if [ ${tmp_exit_code} -eq 0 ]; then
@@ -77,8 +54,7 @@ else
 fi
 echo "*******dreambooth lora infer end***********"
 
-# # 查看结果
-# cat ${log_dir}/ce_res.log
+
 rm -rf ${work_path}/dogs/
 
 echo exit_code:${exit_code}
