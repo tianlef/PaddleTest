0364d3940780381638d57d2ac70963b36259bbda
tianlef <1095012807@qq.com>
2025-04-17 19:55:15 +0800

    ppdiffusers cogvideo a100

--- models/PaddleMIX/CE/ppdiffusers/cogvideo/start.sh
+++ models/PaddleMIX/CE/ppdiffusers/cogvideo/start.sh
@@ -42,44 +42,6 @@ else
 fi
 echo "*******${model_name}_${case_name} end***********"
 
-cd ${work_path1}
-case_name="lora_train"
-
-echo "******* ${model_name}_${case_name} begin***********"
-(bash lora.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******${model_name}_${case_name} end***********"
-
-
-case_name="lora_train_afer_infer"
-
-echo "******* ${model_name}_${case_name} begin***********"
-(python examples/cogvideo/scripts/lora_infer.py \
-      --model_path THUDM/CogVideoX-2b \
-      --prompt "a bear is walking in a zoon" \
-      --lora_path ./cogvideox-lora/checkpoint-10 \
-      --output_path output.mp4) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******${model_name}_${case_name} end***********"
-
-
-
-
-# # 查看结果
-# cat ${log_dir}/ce_res.log
-
 cd ${root_path}/PaddleMIX/ppdiffusers
 
 rm -rf davis_validation_for_cogvideox.tar
