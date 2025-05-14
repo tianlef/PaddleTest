8795de913e6f580364a517e8e1f64fc3b792e984
tianlef <1095012807@qq.com>
2025-05-14 20:41:10 +0800

    fix class

--- models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/test_0_run_train_dit_trainer_auto.sh
+++ models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/test_0_run_train_dit_trainer_auto.sh
@@ -1,4 +1,4 @@
 sed -i 's|per_device_train_batch_size=32|per_device_train_batch_size=16|' 0_run_train_dit_trainer_auto.sh
-sed -i 's"max_steps=7000000|max_steps=10|' 0_run_train_dit_trainer_auto.sh
+sed -i 's|max_steps=7000000|max_steps=10|' 0_run_train_dit_trainer_auto.sh
 sed -i 's|save_steps=5000|save_steps=10|' 0_run_train_dit_trainer_auto.sh
 sh 0_run_train_dit_trainer_auto.sh
