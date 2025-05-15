1052bd64e0b796cb14d457c1ffedb1bb00f68de0
tianlef <1095012807@qq.com>
2025-05-15 16:57:36 +0800

    total dit

--- models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/start.sh
+++ models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/start.sh
@@ -21,63 +21,63 @@ exit_code=0
 # 下载依赖和数据
 bash prepare.sh
 
-# echo "*******class_conditional_image_generation/DiT train begin***********"
-# (bash test_0_run_train_dit_trainer.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "class_conditional_image_generation/DiT train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "class_conditional_image_generation/DiT train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******class_conditional_image_generation/DiT train end***********"
+echo "*******class_conditional_image_generation/DiT train begin***********"
+(bash test_0_run_train_dit_trainer.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "class_conditional_image_generation/DiT train run success" >>"${log_dir}/ce_res.log"
+else
+    echo "class_conditional_image_generation/DiT train run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******class_conditional_image_generation/DiT train end***********"
 
 
-# echo "*******class_conditional_image_generation/DiT multi_train begin***********"
-# (python -u check_loss.py "bash test_1_run_train_dit_notrainer.sh") 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_multi_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "class_conditional_image_generation/DiT multi train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "class_conditional_image_generation/DiT multi train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******class_conditional_image_generation/DiT multi train end***********"
+echo "*******class_conditional_image_generation/DiT multi_train begin***********"
+(python -u check_loss.py "bash test_1_run_train_dit_notrainer.sh") 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_multi_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "class_conditional_image_generation/DiT multi train run success" >>"${log_dir}/ce_res.log"
+else
+    echo "class_conditional_image_generation/DiT multi train run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******class_conditional_image_generation/DiT multi train end***********"
 
-# echo "*******class_conditional_image_generation/DiT auto_train begin***********"
-# (bash test_0_run_train_dit_trainer_auto.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_auto_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "class_conditional_image_generation/DiT auto train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "class_conditional_image_generation/DiT auto train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******class_conditional_image_generation/DiT auto train end***********"
-
-
-# echo "*******class_conditional_image_generation/DiT large_train begin***********"
-# (sh test_4_run_train_largedit_3b_trainer_auto.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_large_train.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "class_conditional_image_generation/DiT large train run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "class_conditional_image_generation/DiT large train run fail" >>"${log_dir}/ce_res.log"
-# fi
-# echo "*******class_conditional_image_generation/DiT large train end***********"
+echo "*******class_conditional_image_generation/DiT auto_train begin***********"
+(bash test_0_run_train_dit_trainer_auto.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_auto_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "class_conditional_image_generation/DiT auto train run success" >>"${log_dir}/ce_res.log"
+else
+    echo "class_conditional_image_generation/DiT auto train run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******class_conditional_image_generation/DiT auto train end***********"
 
 
+echo "*******class_conditional_image_generation/DiT large_train begin***********"
+(sh test_4_run_train_largedit_3b_trainer_auto.sh) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_large_train.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "class_conditional_image_generation/DiT large train run success" >>"${log_dir}/ce_res.log"
+else
+    echo "class_conditional_image_generation/DiT large train run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******class_conditional_image_generation/DiT large train end***********"
 
-# echo "*******class_conditional_image_generation/DiT infer begin***********"
-# (python infer_demo_dit.py) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_infer.log
-# tmp_exit_code=${PIPESTATUS[0]}
-# exit_code=$(($exit_code + ${tmp_exit_code}))
-# if [ ${tmp_exit_code} -eq 0 ]; then
-#     echo "class_conditional_image_generation/DiT infer run success" >>"${log_dir}/ce_res.log"
-# else
-#     echo "class_conditional_image_generation/DiT infer run fail" >>"${log_dir}/ce_res.log"
-# fi
+
+
+echo "*******class_conditional_image_generation/DiT infer begin***********"
+(python infer_demo_dit.py) 2>&1 | tee ${log_dir}/class_conditional_image_generation_DiT_infer.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "class_conditional_image_generation/DiT infer run success" >>"${log_dir}/ce_res.log"
+else
+    echo "class_conditional_image_generation/DiT infer run fail" >>"${log_dir}/ce_res.log"
+fi
 
 echo "*******class_conditional_image_generation/DiT transfer begin***********"
 (python tools/convert_dit_to_ppdiffusers.py \
