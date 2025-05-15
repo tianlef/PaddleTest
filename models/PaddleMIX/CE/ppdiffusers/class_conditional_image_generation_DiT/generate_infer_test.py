f4af72cfdb1ca64b62a274c4d8fb02d7c1bcddd1
tianlef <1095012807@qq.com>
2025-05-15 16:20:21 +0800

    fix dit

--- models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/generate_infer_test.py
+++ models/PaddleMIX/CE/ppdiffusers/class_conditional_image_generation_DiT/generate_infer_test.py
@@ -1,10 +1,25 @@
+import json
+import glob
 import paddle
 from paddlenlp.trainer import set_seed
 
 from ppdiffusers import DDIMScheduler, DiTPipeline
 
+# 合并json文件
+merged_json = {}
+with open("output_trainer/DiT_XL_patch2_trainer/checkpoint-10/model_index.json", "r") as f:
+    model_json = json.load(f)
+    merged_json.update(model_json)
+with open("tools/ImageNet_id2label.json", "r") as f:
+    label_json = json.load(f)
+    merged_json.update(label_json)
+
+with open("output_trainer/DiT_XL_patch2_trainer/checkpoint-10/model_index.json", "w") as f:
+    json.dump(merged_json, f, indent=4, ensure_ascii=False)
+
+# 执行推理
 dtype = paddle.float32
-pipe = DiTPipeline.from_pretrained("./DiT_XL_2_256", paddle_dtype=dtype)
+pipe = DiTPipeline.from_pretrained("output_trainer/DiT_XL_patch2_trainer/checkpoint-10", paddle_dtype=dtype)
 pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 
 words = ["golden retriever"]  # class_ids [207]
