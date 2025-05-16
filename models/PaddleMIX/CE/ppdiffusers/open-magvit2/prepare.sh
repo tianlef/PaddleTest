a8f1c8d4d78591fde33b548bc847a89d99a14d4e
tianlef <1095012807@qq.com>
2025-05-16 17:23:12 +0800

    fix open-magvit2

--- models/PaddleMIX/CE/ppdiffusers/open-magvit2/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/open-magvit2/prepare.sh
@@ -2,6 +2,6 @@ prepare_path=${root_path}/PaddleMIX/ppdiffusers/examples/visual_tokenizer
 
 cd $prepare_path
 rm -rf open-magvitv2.tar
-wget ${base_url}/mix_data/open-magvitv2.tar --no-proxy
+wget ${base_url}/open-magvitv2.tar --no-proxy
 tar -xf open-magvitv2.tar
 
