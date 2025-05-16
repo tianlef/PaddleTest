44e1f517bbd8c3957d5ab8e6bc62806d2cdae698
tianlef <1095012807@qq.com>
2025-05-16 18:35:19 +0800

    open magvit

--- models/PaddleMIX/CE/ppdiffusers/open-magvit2/start_a100.sh
+++ models/PaddleMIX/CE/ppdiffusers/open-magvit2/start_a100.sh
@@ -47,6 +47,29 @@ else
 fi
 echo "******* ${model_name}_${case_name} end***********"
 
+case_name=train_single_gpu_256
+echo "******* ${model_name}_${case_name} begin***********"
+(python -u check_loss.py "python train_tokenizer.py --config imagenet_lfqgan_256_L.yaml") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "******* ${model_name}_${case_name} end***********"
+
+case_name=train_multiple_gpu_256
+echo "******* ${model_name}_${case_name} begin***********"
+(python -u check_loss.py "python -u  -m paddle.distributed.launch --gpus "0,1,2,3" train_tokenizer.py  --config imagenet_lfqgan_256_L.yaml") 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "******* ${model_name}_${case_name} end***********"
 
 case_name=infer_256
 
