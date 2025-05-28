75eb729fd2fd8b186153019445934c3508cc04c8
tianlef <1095012807@qq.com>
2025-05-28 19:46:03 +0800

    fix InternVL2 prepare

--- models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
@@ -3,14 +3,3 @@ pip install soundfile
 pip install decord
 
 
-
-
-rm -rf playground
-mkdir playground
-cd playground
-mkdir data 
-wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
-tar -xf chartqa.tar -C data
-wget https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource.tar
-tar -xf opensource.tar
-
