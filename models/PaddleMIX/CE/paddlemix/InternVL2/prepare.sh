06e0f4d1179e1616797da2e71efa04dd0123076a
tianlef <1095012807@qq.com>
2025-05-29 18:52:32 +0800

    fix download.sh

--- models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
@@ -11,17 +11,16 @@ bash download.sh https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/LLaVA
 # wget --progress=dot:mega -O internvl2_download.log https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/LLaVA-Pretrain.tar # 27 G
 tar -xf LLaVA-Pretrain.tar
 cd LLaVA-Pretrain
-bash download.sh https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/blip_laion_cc_sbu_558k.jsonl # 2.5 G
-# wget --progress=dot:mega -O internvl2_download.log   https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/blip_laion_cc_sbu_558k.jsonl
+wget https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/blip_laion_cc_sbu_558k.jsonl
 
+cd ${root_path}/PaddleMIX
 # sft/lora model data
-rm -rf playground
-mkdir playground
-cd playground
-mkdir data 
-bash download.sh https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
-# wget --progress=dot:mega -O internvl2_download.log  https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
-tar -xf chartqa.tar -C data
+bash download.sh https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar 
 bash download.sh https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource.tar
+rm -rf playground
+mkdir -p playground/data
+# wget --progress=dot:mega -O internvl2_download.log  https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
+tar -xf chartqa.tar -C playground/data
+
 # wget --progress=dot:mega -O internvl2_download.log  https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource.tar
-tar -xf opensource.tar
+tar -xf opensource.tar -C playground
