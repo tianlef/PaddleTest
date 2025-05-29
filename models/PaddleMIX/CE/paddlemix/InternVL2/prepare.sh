b33a77572cce0d15345c41353742c64f0973c28f
tianlef <1095012807@qq.com>
2025-05-29 17:33:02 +0800

    internvl gpu download

--- models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
+++ models/PaddleMIX/CE/paddlemix/InternVL2/prepare.sh
@@ -5,16 +5,16 @@ pip install decord
 
 # pretrain model data
 cd ${root_path}/PaddleMIX
-wget --quiet https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/LLaVA-Pretrain.tar # 27 G
+wget  --progress=dot:10 https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/LLaVA-Pretrain.tar # 27 G
 tar -xf LLaVA-Pretrain.tar
 cd LLaVA-Pretrain
-wget --quiet https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/blip_laion_cc_sbu_558k.jsonl
+wget --progress=dot:10  https://paddlenlp.bj.bcebos.com//datasets/paddlemix/LLaVA/blip_laion_cc_sbu_558k.jsonl
 
 # sft/lora model data
 mkdir playground
 cd playground
 mkdir data 
-wget --quiet https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
+wget --progress=dot:10 https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/data/chartqa.tar
 tar -xf chartqa.tar -C data
-wget --quiet https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource.tar
+wget --progress=dot:10 https://paddlenlp.bj.bcebos.com/datasets/paddlemix/playground/opensource.tar
 tar -xf opensource.tar
