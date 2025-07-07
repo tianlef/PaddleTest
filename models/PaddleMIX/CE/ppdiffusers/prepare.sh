b4c5eb9812566ec10b610cf35e713e799c2bceb6
tianlef <1095012807@qq.com>
2025-07-07 17:17:35 +0800

    fix ppdiffusers inference prepare

--- models/PaddleMIX/CE/ppdiffusers/prepare.sh
+++ models/PaddleMIX/CE/ppdiffusers/prepare.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-pip install pytest safetensors ftfy fastcore opencv-python einops parameterized requests-mock
+python -m pip install --upgrade pip
 
+pip install pytest safetensors ftfy fastcore opencv-python einops parameterized requests-mock
+pip install ligo-segments
+
+pip install fastdeploy-gpu-python -f https://www.paddlepaddle.org.cn/whl/fastdeploy.html
 work_path2=${root_path}/PaddleMIX/ppdiffusers/
 echo ${work_path2}/
 
