eb0bda12adb2fca5b1579ee00639b2409f0688fc
tianlef <1095012807@qq.com>
2025-07-15 14:33:37 +0800

    ppdiffusers release deploy

--- models/PaddleMIX/CE/ppdiffusers/deploy/script/release.sh
+++ models/PaddleMIX/CE/ppdiffusers/deploy/script/release.sh
@@ -19,74 +19,76 @@ exit_code=0
 
 cd ${work_path}
 
-# test_list=(
-#     "controlnet/"
-# )
-# for subdir in */; do
-#   if [ -d "$subdir" ]; then
-#     echo "Testing $subdir"
-#     found=false
-#     for var in "${test_list[@]}"; do 
-#       if [[ "$subdir" == "$var" ]]; then
-#         found=true
-#         break
-#       fi
-#     done
-#     if $found; then
-#       cd "$subdir"
-#       echo "Copying test scripts to $subdir"
-#       cp -f ../test_*.sh .
-#       clean_name="${subdir%/}"
-#       bash test_paddle.sh > ${log_dir}/${clean_name}_paddle.log 2>&1
-#       tmp_exit_code=${PIPESTATUS[0]}
-#       exit_code=$((exit_code + ${tmp_exit_code}))
-#       if [ ${tmp_exit_code} -eq 0 ]; then
-#         echo "${clean_name}_paddle success" >>"${log_dir}/ce_res.log"
-#       else
-#         echo "${clean_name}_paddle fail" >>"${log_dir}/ce_res.log"
-#       fi
-#       bash test_paddle_tensorrt.sh > ${log_dir}/${clean_name}_paddle_tensorrt.log 2>&1
-#       tmp_exit_code=${PIPESTATUS[0]}
-#       exit_code=$((exit_code + ${tmp_exit_code}))
-#       if [ ${tmp_exit_code} -eq 0 ]; then
-#         echo "${clean_name}_paddle_tensorrt success" >>"${log_dir}/ce_res.log"
-#       else
-#           echo "${clean_name}_paddle_tensorrt fail" >>"${log_dir}/ce_res.log"
-#       fi
-#       cd ..
-#     fi
-#     else
-#         echo "subdir is not a test directory: $subdir"
-#     fi
-# done
-
-cd ${work_path}/ipadapter
+test_list=(
+    "controlnet/"
+    "sd15/"
+    "sdxl/"
+)
 for subdir in */; do
   if [ -d "$subdir" ]; then
     echo "Testing $subdir"
-    cd "$subdir"
-    echo "Copying test scripts to $subdir"
-    cp -f ../test_*.sh . 
-    clean_name="${subdir%/}"
-    bash test_paddle.sh > ${log_dir}/ipadapter_${clean_name}_paddle.log 2>&1
-    tmp_exit_code=${PIPESTATUS[0]}
-    exit_code=$((exit_code + ${tmp_exit_code}))
-    if [ ${tmp_exit_code} -eq 0 ]; then
-      echo "ipadapter_${clean_name}_paddle success" >>"${log_dir}/ce_res.log"
-    else
-        echo "ipadapter_${clean_name}_paddle fail" >>"${log_dir}/ce_res.log"
+    found=false
+    for var in "${test_list[@]}"; do 
+      if [[ "$subdir" == "$var" ]]; then
+        found=true
+        break
+      fi
+    done
+    if $found; then
+      cd "$subdir"
+      echo "Copying test scripts to $subdir"
+      cp -f ../test_*.sh .
+      clean_name="${subdir%/}"
+      bash test_paddle.sh > ${log_dir}/${clean_name}_paddle.log 2>&1
+      tmp_exit_code=${PIPESTATUS[0]}
+      exit_code=$((exit_code + ${tmp_exit_code}))
+      if [ ${tmp_exit_code} -eq 0 ]; then
+        echo "${clean_name}_paddle success" >>"${log_dir}/ce_res.log"
+      else
+        echo "${clean_name}_paddle fail" >>"${log_dir}/ce_res.log"
+      fi
+      bash test_paddle_tensorrt.sh > ${log_dir}/${clean_name}_paddle_tensorrt.log 2>&1
+      tmp_exit_code=${PIPESTATUS[0]}
+      exit_code=$((exit_code + ${tmp_exit_code}))
+      if [ ${tmp_exit_code} -eq 0 ]; then
+        echo "${clean_name}_paddle_tensorrt success" >>"${log_dir}/ce_res.log"
+      else
+          echo "${clean_name}_paddle_tensorrt fail" >>"${log_dir}/ce_res.log"
+      fi
+      cd ..
     fi
-    bash test_paddle_tensorrt.sh > ${log_dir}/ipadapter_${clean_name}_paddle_tensorrt.log 2>&1
-    tmp_exit_code=${PIPESTATUS[0]}
-    exit_code=$((exit_code + ${tmp_exit_code}))
-    if [ ${tmp_exit_code} -eq 0 ]; then
-      echo "ipadapter_${clean_name}_paddle_tensorrt success" >>"${log_dir}/ce_res.log"
     else
-        echo "ipadapter_${clean_name}_paddle_tensorrt fail" >>"${log_dir}/ce_res.log"
+        echo "subdir is not a test directory: $subdir"
     fi
-    cd ..
-  fi
 done
+
+# cd ${work_path}/ipadapter
+# for subdir in */; do
+#   if [ -d "$subdir" ]; then
+#     echo "Testing $subdir"
+#     cd "$subdir"
+#     echo "Copying test scripts to $subdir"
+#     cp -f ../test_*.sh . 
+#     clean_name="${subdir%/}"
+#     bash test_paddle.sh > ${log_dir}/ipadapter_${clean_name}_paddle.log 2>&1
+#     tmp_exit_code=${PIPESTATUS[0]}
+#     exit_code=$((exit_code + ${tmp_exit_code}))
+#     if [ ${tmp_exit_code} -eq 0 ]; then
+#       echo "ipadapter_${clean_name}_paddle success" >>"${log_dir}/ce_res.log"
+#     else
+#         echo "ipadapter_${clean_name}_paddle fail" >>"${log_dir}/ce_res.log"
+#     fi
+#     bash test_paddle_tensorrt.sh > ${log_dir}/ipadapter_${clean_name}_paddle_tensorrt.log 2>&1
+#     tmp_exit_code=${PIPESTATUS[0]}
+#     exit_code=$((exit_code + ${tmp_exit_code}))
+#     if [ ${tmp_exit_code} -eq 0 ]; then
+#       echo "ipadapter_${clean_name}_paddle_tensorrt success" >>"${log_dir}/ce_res.log"
+#     else
+#         echo "ipadapter_${clean_name}_paddle_tensorrt fail" >>"${log_dir}/ce_res.log"
+#     fi
+#     cd ..
+#   fi
+# done
 
 bash gather_img_video_to_one_file.sh
 cat ${log_dir}/ce_res.log
