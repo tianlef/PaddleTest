0df34c47e8dd037d6f81400ee61a7f7cb031af40
tianlef <1095012807@qq.com>
2025-07-16 14:41:27 +0800

    trainging-free

---
+++ models/PaddleMIX/CE/ppdiffusers/training-free-tgate/start_a100.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+cur_path=$(pwd)
+echo ${cur_path}
+
+work_path=${root_path}/PaddleMIX/ppdiffusers/examples/Fast-Diffusers/Training-Free/tgate
+echo ${work_path}
+
+
+log_dir=${root_path}/ppdiffusers_log
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+/bin/cp -rf ./* ${work_path}
+
+cd ${work_path}
+exit_code=0
+
+model_name="trainfree-tgate"
+case_name="sdxl" 
+
+echo "*******${model_name}_{case_name} begin***********"
+(bash sdxl.sh) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+
+case_name="Pixart-Alpha" 
+
+echo "*******${model_name}_{case_name} begin***********"
+(bash pixart_alpha.sh) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+
+case_name="LCM-SDXL" 
+
+echo "*******${model_name}_{case_name} begin***********"
+(bash lcm_sdxl.sh) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+echo exit_code:${exit_code}
+exit ${exit_code}
---
+++ models/PaddleMIX/CE/ppdiffusers/training-free/start_a100.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+cur_path=$(pwd)
+echo ${cur_path}
+
+work_path=${root_path}/PaddleMIX/ppdiffusers/examples/Fast-Diffusers/Training-Free
+echo ${work_path}
+
+
+log_dir=${root_path}/ppdiffusers_log
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
+/bin/cp -rf ./* ${work_path}
+
+cd ${work_path}
+exit_code=0
+
+model_name="trainfree"
+
+
+case_name="blockdance" 
+
+echo "*******${model_name}_{case_name} begin***********"
+(cd ${work_path}/blockdance && python text_to_image_generation-flux-blockdance.py) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+
+
+case_name="pab" 
+
+echo "*******${model_name}_{case_name} begin***********"
+(cd ${work_path}/pab && python text_to_image_generation-flux-pab.py) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+case_name="sortblock" 
+echo "*******${model_name}_{case_name} begin***********"
+(cd ${work_path}/sortblock && python diffusers_sorttaylor_flux.py ) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+
+case_name="taylorseer" 
+echo "*******${model_name}_{case_name} begin***********"
+(cd ${work_path}/taylorseer && python diffusers_taylorseer_flux.py) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+
+case_name="teacache" 
+echo "*******${model_name}_{case_name} begin***********"
+(cd ${work_path}/teacache && python text_to_image_generation-flux-teacache.py) 2>&1 | tee ${log_dir}/${model_name}_{case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_{case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_{case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_{case_name} end***********"
+
+echo exit_code:${exit_code}
+exit ${exit_code}
