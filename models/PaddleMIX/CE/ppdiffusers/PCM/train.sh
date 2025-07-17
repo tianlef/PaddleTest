652d2ae4ea927aa3f5270befd9ea73a92c5b5b03
tianlef <1095012807@qq.com>
2025-07-17 20:35:56 +0800

    fix pcm

--- models/PaddleMIX/CE/ppdiffusers/PCM/train.sh
+++ models/PaddleMIX/CE/ppdiffusers/PCM/train.sh
@@ -1,5 +1,5 @@
 python -u -m paddle.distributed.launch --gpus "0,1,2,3,4,5,6,7" train_pcm_lora_sd3_adv.py \
-    --data_path "./cc3m/cc3m-train-0000" \
+    --data_path "./cc3m" \
     --pretrained_teacher_model stabilityai/stable-diffusion-3-medium-diffusers \
     --output_dir "outputs/lora_64_fuyun_PCM_sd3_202503191011" \
     --tracker_project_nam "lora_64_formal_fuyun_PCM_sd3_202503191011" \
@@ -9,12 +9,12 @@ python -u -m paddle.distributed.launch --gpus "0,1,2,3,4,5,6,7" train_pcm_lora_s
     --learning_rate "5e-6" \
     --loss_type "huber" \
     --adam_weight_decay "1e-3" \
-    --max_train_steps "10000" \
+    --max_train_steps "20000" \
     --dataloader_num_workers "16" \
     --w_min "4" \
     --w_max "5" \
-    --validation_steps "5000" \
-    --checkpointing_steps "5000" \
+    --validation_steps "1000" \
+    --checkpointing_steps "2000" \
     --checkpoints_total_limit "10" \
     --train_batch_size "2" \
     --gradient_accumulation_steps "1" \
@@ -23,4 +23,5 @@ python -u -m paddle.distributed.launch --gpus "0,1,2,3,4,5,6,7" train_pcm_lora_s
     --num_euler_timesteps "100" \
     --multiphase "4" \
     --adv_weight "0.1" \
-    --adv_lr "1e-5"
+    --adv_lr "1e-5" \
+    --report_to wandb \
