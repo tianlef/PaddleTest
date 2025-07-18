24ea74c2d350ae73e010ddee633f2d6207589cee
tianlef <1095012807@qq.com>
2025-07-18 16:45:11 +0800

    fix dmd2

--- models/PaddleMIX/CE/ppdiffusers/dmd2/start_a100.sh
+++ models/PaddleMIX/CE/ppdiffusers/dmd2/start_a100.sh
@@ -62,18 +62,18 @@ model_name="dmd2"
 # echo "*******${model_name}_${case_name} end***********"
 
 
-case_name="train_lora"
+# case_name="train_lora"
  
-echo "*******${model_name}_${case_name} begin***********"
-(bash train_lora.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******${model_name}_${case_name} end***********"
+# echo "*******${model_name}_${case_name} begin***********"
+# (bash train_lora.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "*******${model_name}_${case_name} end***********"
 
 
 # case_name="infer_lora"
