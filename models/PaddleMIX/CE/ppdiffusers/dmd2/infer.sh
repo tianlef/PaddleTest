7b07d995e442356cd88807962ecaeddddee7069a
tianlef <1095012807@qq.com>
2025-07-18 19:48:06 +0800

    fix dmd2

--- models/PaddleMIX/CE/ppdiffusers/dmd2/infer.sh
+++ models/PaddleMIX/CE/ppdiffusers/dmd2/infer.sh
@@ -1,5 +1,5 @@
 python -u edm/test_folder_edm.py \
-    --folder output/imagenet_gan_classifier_genloss3e-3_diffusion1000_lr2e-6_scratch/time_1752744858_seed1 \
+    --folder output/imagenet_gan_classifier_genloss3e-3_diffusion1000_lr2e-6_scratch/time_1752837990_seed1 \
     --wandb_name test_imagenet_gan_classifier_genloss3e-3_diffusion1000_lr2e-6_scratch \
     --wandb_entity jll-none \
     --wandb_project dmd2 \
