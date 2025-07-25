a5709b410eaacd0442424004444fb195ef5309aa
tianlef <1095012807@qq.com>
2025-07-25 20:27:53 +0800

    ix get data for fastdeploy

--- models/PaddleMIX/CE/ppdiffusers/flux_lighting/start_a100.sh
+++ models/PaddleMIX/CE/ppdiffusers/flux_lighting/start_a100.sh
@@ -28,6 +28,19 @@ exit_code=0
 model_name="flux-lightning"
 
 
+case_name="train_single" 
+
+echo "*******${model_name}_${case_name} begin***********"
+(bash train_single.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+tmp_exit_code=${PIPESTATUS[0]}
+exit_code=$(($exit_code + ${tmp_exit_code}))
+if [ ${tmp_exit_code} -eq 0 ]; then
+    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+else
+    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+fi
+echo "*******${model_name}_${case_name} end***********"
+
 case_name="train" 
 
 echo "*******${model_name}_${case_name} begin***********"
@@ -44,31 +57,31 @@ echo "*******${model_name}_${case_name} end***********"
 # rm -rf paddle_lora_weights.safetensors
 # wget https://dataset.bj.bcebos.com/PaddleMIX/flux-lightning/202507112228_latest/paddle_lora_weights.safetensors
 
-case_name="infer" 
+# case_name="infer" 
 
-echo "*******${model_name}_${case_name} begin***********"
-(bash infer.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******${model_name}_${case_name} end***********"
+# echo "*******${model_name}_${case_name} begin***********"
+# (bash infer.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "*******${model_name}_${case_name} end***********"
 
-case_name="infer_speed" 
+# case_name="infer_speed" 
 
-echo "*******${model_name}_${case_name} begin***********"
-(bash infer_speed.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
-tmp_exit_code=${PIPESTATUS[0]}
-exit_code=$(($exit_code + ${tmp_exit_code}))
-if [ ${tmp_exit_code} -eq 0 ]; then
-    echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
-else
-    echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
-fi
-echo "*******${model_name}_${case_name} end***********"
+# echo "*******${model_name}_${case_name} begin***********"
+# (bash infer_speed.sh) 2>&1 | tee ${log_dir}/${model_name}_${case_name}.log
+# tmp_exit_code=${PIPESTATUS[0]}
+# exit_code=$(($exit_code + ${tmp_exit_code}))
+# if [ ${tmp_exit_code} -eq 0 ]; then
+#     echo "${model_name}_${case_name} run success" >>"${log_dir}/ce_res.log"
+# else
+#     echo "${model_name}_${case_name} run fail" >>"${log_dir}/ce_res.log"
+# fi
+# echo "*******${model_name}_${case_name} end***********"
 
 # rm -rf paddle_lora_weights.safetensors
 # rm -rf laion-45w
