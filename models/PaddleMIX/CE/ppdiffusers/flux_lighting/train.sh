d75b8f454ba7d882691a8405b242743cd09f5197
tianlef <1095012807@qq.com>
2025-07-25 17:11:52 +0800

    add release

--- models/PaddleMIX/CE/ppdiffusers/flux_lighting/train.sh
+++ models/PaddleMIX/CE/ppdiffusers/flux_lighting/train.sh
@@ -1,6 +1,6 @@
 export FLAGS_use_fused_rmsnorm="yes"
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python -u -m paddle.distributed.launch --gpus "1" train_flux_lightning_lora.py \
+python -u -m paddle.distributed.launch --gpus "0,1,2,3,4,5,6,7" train_flux_lightning_lora.py \
     --data_path "./lain-10" \
     --file_list_path "./file_list_10.txt" \
     --pretrained_teacher_model "black-forest-labs/FLUX.1-dev" \
