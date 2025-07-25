ea61205434fc92a37a89cff1906796423cd37047
tianlef <1095012807@qq.com>
2025-07-25 18:35:05 +0800

    add qwen25vl

---
+++ models/PaddleMIX/CE/paddlemix/fire_release_a100.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+exit_code=0
+
+log_dir=${root_path}/paddlemix_log
+
+
+work_path=$(pwd)
+echo ${work_path}
+
+#bash prepare.sh
+
+cd ${work_path}
+test_list=(
+    "qwen2.5vl/"
+)
+# 遍历当前目录下的子目录
+for subdir in */; do
+  if [ -d "$subdir" ]; then
+
+    echo "subdir: $subdir"
+    found=false
+    for var in "${test_list[@]}"; do 
+      if [[ "$subdir" == "$var" ]]; then
+        found=true
+        break
+      fi
+    done
+
+    if $found; then
+      start_script_path="$subdir/start_a100.sh"
+      if [ -f "$start_script_path" ]; then
+        echo "start_script_path: $start_script_path"
+        cd $subdir
+        bash start_a100.sh
+        exit_code=$((exit_code + $?))
+        cd ..
+      fi
+    fi
+    else
+        echo "subdir is not a test directory: $subdir"
+    fi
+done
+
+echo "exit code: $exit_code"
+
+# 查看结果
+cat ${log_dir}/ce_res.log
